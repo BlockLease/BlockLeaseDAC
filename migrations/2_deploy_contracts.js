bb9a4afb9e07b1a9ecd516aa7b4b161562be0b59
Chance Hudson <jchancehud@gmail.com>
2018-02-23 16:59:14 -0600

    Update build system to include crowdsale registry in BlockLeaseDAC constructor

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,9 @@
 'use strict';
 
 const BlockLeaseDAC = artifacts.require('BlockLeaseDAC.sol');
-const CrowdsaleRegistry = artifacts.require('CrowdsaleEntry.sol');
+const CrowdsaleRegistry = artifacts.require('CrowdsaleRegistry.sol');
 
 module.exports = async function(deployer) {
-  const deployed = deployer.deploy(CrowdsaleRegistry);
-  console.log(deployed);
-  return await deployer.deploy(BlockLeaseDAC);
-  // return await deployer.deploy(BlockLeaseDAC);
+  const registry = await CrowdsaleRegistry.deployed();
+  return await deployer.deploy(BlockLeaseDAC, registry.address);
 };
