b0720af57356528e7be8b5c2a5482b196bd9714b
Chance Hudson <jchancehud@gmail.com>
2018-02-26 16:20:17 -0600

    Created constants for accessing proposal fields

--- test/proposals.js
+++ test/proposals.js
@@ -3,6 +3,13 @@
 const CrowdsaleRegistry = artifacts.require('./CrowdsaleRegistry.sol');
 const BlockLeaseDAC = artifacts.require('./BlockLeaseDAC.sol');
 
+const TOKENS_FOR_SALE_INDEX = 0;
+const TOKENS_PER_ETH_INDEX = 1;
+const BONUS_POOL_INDEX = 2;
+const DEV_POOL_INDEX = 3;
+const VOTING_BLOCK_COUNT_INDEX = 4;
+const BLOCK_NUMBER_INDEX = 5;
+
 async function bootstrap(dac, operator) {
   return throwToBool(async () => {
     await dac.bootstrap(0, await dac.totalSupply.call(), 0, 2, {
@@ -50,7 +57,7 @@ async function waitForBlock(num) {
 async function votingEndBlock(dac) {
   const votingBlockCount = await dac.votingBlockCount.call();
   const proposal = await activeProposal(dac);
-  return +proposal[5] + +votingBlockCount;
+  return +proposal[BLOCK_NUMBER_INDEX] + +votingBlockCount;
 }
 
 async function activeProposal(dac) {
@@ -112,9 +119,11 @@ contract('BlockLeaseDAC', (accounts) => {
     const tokensForSale = await dac.tokensForSale.call();
     const tokensPerEth = await dac.tokensPerEth.call();
     const bonusPool = await dac.bonusPool.call();
-    assert.equal(tokensForSale.toString(), proposal[0].toString(), 'Tokens for sale is incorrect');
-    assert.equal(tokensPerEth.toString(), proposal[1].toString(), 'Tokens per eth is incorrect');
-    assert.equal(bonusPool.toString(), proposal[2].toString(), 'Bonus pool is incorrect');
+    const devPool = await dac.devPool.call();
+    assert.equal(tokensForSale.toString(), proposal[TOKENS_FOR_SALE_INDEX].toString(), 'Tokens for sale is incorrect');
+    assert.equal(tokensPerEth.toString(), proposal[TOKENS_PER_ETH_INDEX].toString(), 'Tokens per eth is incorrect');
+    assert.equal(bonusPool.toString(), proposal[BONUS_POOL_INDEX].toString(), 'Bonus pool is incorrect');
+    assert.equal(devPool.toString(), proposal[DEV_POOL_INDEX].toString(), 'Dev pool is incorrect');
   });
 
   it('should fail to create a new proposal if not operator', async () => {
@@ -126,36 +135,38 @@ contract('BlockLeaseDAC', (accounts) => {
   it('should fail to create proposal with lower tokensForSale value', async () => {
     const dac = await BlockLeaseDAC.deployed();
     const proposal = await activeProposal(dac);
-    proposal[0] = proposal[0] - 1;
+    proposal[TOKENS_FOR_SALE_INDEX] = proposal[TOKENS_FOR_SALE_INDEX] - 1;
     assert(!await createProposal(dac, accounts[0], proposal), 'Decreased tokensForSale in proposal');
   });
 
   it('should fail to create proposal with higher tokensPerEth value', async () => {
     const dac = await BlockLeaseDAC.deployed();
     const proposal = await activeProposal(dac);
-    proposal[1] = +proposal[1] + 1;
+    proposal[TOKENS_PER_ETH_INDEX] = +proposal[TOKENS_PER_ETH_INDEX] + 1;
     assert(!await createProposal(dac, accounts[0], proposal, 'Increased tokensPerEth in proposal'));
   });
 
   it('should fail to create proposal with lower bonusPool value', async () => {
     const dac = await BlockLeaseDAC.deployed();
     const proposal = await activeProposal(dac);
-    proposal[2] = +proposal[2] - 1;
+    proposal[BONUS_POOL_INDEX] = +proposal[BONUS_POOL_INDEX] - 1;
     assert(!await createProposal(dac, accounts[0], proposal, 'Decreased bonusPool in proposal'));
   });
 
   it('should fail to create proposal with lower devPool value', async () => {
     const dac = await BlockLeaseDAC.deployed();
     const proposal = await activeProposal(dac);
-    proposal[3] = +proposal[3] - 1;
+    proposal[DEV_POOL_INDEX] = +proposal[DEV_POOL_INDEX] - 1;
     assert(!await createProposal(dac, accounts[0], proposal, 'Decreased bonusPool in proposal'));
   });
 
-  it('should fail to create proposal with 0 proposalVotingTime', async () => {
+  it('should fail to create proposal with votingBlockCount <= 1', async () => {
     const dac = await BlockLeaseDAC.deployed();
     const proposal = await activeProposal(dac);
-    proposal[4] = 0;
-    assert(!await createProposal(dac, accounts[0], proposal, 'Set proposalVotingTime to 0 in proposal'));
+    proposal[VOTING_BLOCK_COUNT_INDEX] = 0;
+    assert(!await createProposal(dac, accounts[0], proposal, 'Set votingBlockCount to 0 in proposal'));
+    proposal[VOTING_BLOCK_COUNT_INDEX] = 1;
+    assert(!await createProposal(dac, accounts[0], proposal, 'Set votingBlockCount to 1 in proposal'));
   });
 
   it('should create another proposal', async () => {
